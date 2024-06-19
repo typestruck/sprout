ce3c0eebb5092f4440a838ea941db33e10382f44
Eduardo Asafe <e@asafe.dev>
2024-06-19 11:42:52 -0300

    Use mailjet api

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,9 @@
 use postgres::{Client, Error, NoTls};
 
 pub struct User {
+    pub id: u32,
     pub email: String,
-    pub sender: String,
+    pub name: String,
     pub unread_messages: i32
 }
 
@@ -12,9 +13,9 @@ pub fn connect() -> Result<Client, Error> {
 
 pub fn select_users_with_unread_messages(client: &mut Client) -> Result<Vec<User>, Error> {
     let query = "
-        SELECT r.email, s.name, count(1)
+        SELECT r.id, r.email, s.name, count(1)
         FROM messages m JOIN users s ON m.sender = s.id JOIN users r ON m.recipient = r.id
-        WHERE status < 4 AND date >= (utc_now() - INTERVAL '1 hour')
+        WHERE status < 4 AND date >= (utc_now() - INTERVAL '1 hour') AND not(r.temporary) AND r.email LIKE '%@%' AND r.receive_emails < 2
         GROUP BY recipient, sender, r.email, s.name
         ORDER BY recipient
         ";
@@ -22,9 +23,10 @@ pub fn select_users_with_unread_messages(client: &mut Client) -> Result<Vec<User
 
     for row in client.query(query, &[])? {
         users.push(User {
-            email: row.get(0),
-            sender: row.get(1),
-            unread_messages: row.get(2)
+            id : row.get(0),
+            email: row.get(1),
+            name: row.get(2),
+            unread_messages: row.get(3)
         })
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,76 @@
-use postgres::{Error};
+use database::User;
+use itertools::Itertools;
+use mailjet_rs::common::{Payload, Recipient};
+use mailjet_rs::v3::Message;
+use mailjet_rs::{Client, SendAPIVersion};
+use postgres::Error;
+use serde_json::to_string;
+use std::env;
 
 mod database;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let client = Client::new(
+        SendAPIVersion::V3,
+        env::var("MAILJET_PUBLIC_KEY").unwrap().as_str(),
+        env::var("MAILJET_PRIVATE_KEY").unwrap().as_str(),
+    );
+    let emails = build_emails()?;
+
     println!("Checking for emails to send...");
-    notify_unread_messages().expect("Couldn't rebuild suggestions!");
-    println!("Successfully sent emails");
-}
 
-fn notify_unread_messages() -> Result<(), Error> {
-    let mut client = database::connect()?; //poor man's monad
-    let users = database::select_users_with_unread_messages(&mut client)?;
-
-    for u in users {
-
+    if emails.messages.is_empty() {
+        println!("No emails to send");
+    } else {
+        client.send(emails).await.expect("Could not send emails!");
+        println!("Successfully sent emails");
     }
 
-    return Ok(());
+    Ok(())
+}
+
+fn build_emails() -> Result<Batch, Error> {
+    let mut client = database::connect()?; //poor man's monad
+    let users = database::select_users_with_unread_messages(&mut client)?;
+    let mut emails = Vec::new();
+
+    for (id, chunk) in &users.into_iter().chunk_by(|elt| elt.id) {
+        let rows: Vec<User> = chunk.collect();
+        let mut user_names: Vec<String> = Vec::new();
+
+        for r in &rows {
+            user_names.push(format!(" <b>{}</b>", r.name));
+        }
+
+        let count = rows[0].unread_messages;
+        let email_address = &rows[0].email;
+        let mut message = Message::new(
+            "contact@mero.chat",
+            "MeroChat",
+            Some("You have unread messages on MeroChat!".to_string()),
+            Some(format!("Hey there! You have {} unread messages! Go to MeroChat and reply: https://mero.chat/im", count))
+        );
+
+        message.html_part = Some(format!("Hey there! You have {} unread messages from{:?} <br><br><a href ={}>Go to MeroChat and reply</a>", count, user_names, "https://mero.chat/im"));
+        message.push_recipient(Recipient::new(email_address));
+
+        emails.push(message);
+    }
+
+    return Ok(Batch { messages: emails });
+}
+
+struct Batch {
+    pub messages: Vec<Message>,
+}
+
+impl Payload for Batch {
+    fn to_json(&self) -> String {
+        return format!(
+            "{{ {}: {} }}",
+            "Messages",
+            to_string(&self.messages).unwrap()
+        );
+    }
 }
