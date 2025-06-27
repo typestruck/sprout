591458230047da9a224228b7242e0e30844534a2
Eduardo Asafe <e@asafe.dev>
2025-06-27 11:15:59 +0000

    Reset password emails

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,103 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::serde::{Deserialize, json::Json};
 use database::User;
 use itertools::Itertools;
 use mailjet_rs::common::{Payload, Recipient};
 use mailjet_rs::v3::Message;
 use mailjet_rs::{Client, SendAPIVersion};
 use serde_json::{to_string, Map, Value};
+use std::borrow::Cow;
 use std::env;
-use uuid::Uuid;
 use tokio_postgres::Error;
+use uuid::Uuid;
 
 mod database;
 
-//this program will be called every hour to check unread messages
-#[tokio::main]
+#[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.get(1) {
+        Some(arg) => {
+            if arg == "--unread" {
+                unread_emails().await?;
+            } else if arg == "--serve" {
+                serve().await?
+            }
+        }
+        _ => println!("Invalid option. Try unread or serve"),
+    }
+
+    return Ok(());
+}
+
+async fn serve() -> Result<(), rocket::Error> {
+    let figment = rocket::Config::figment().merge(("port", 4000));
+
+    rocket::custom(figment)
+        .mount("/", routes![reset])
+        .launch()
+        .await?;
+
+    return Ok(());
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Reset<'r>  {
+    token: Cow<'r, str>,
+    email: Cow<'r, str>,
+    user_id: u64,
+}
+
+#[post("/reset", data = "<reseter>")]
+async fn reset(reseter : Json<Reset<'_>>) -> Json<()> {
     let client = Client::new(
         SendAPIVersion::V3,
         env::var("MAILJET_PUBLIC_KEY").unwrap().as_str(),
         env::var("MAILJET_PRIVATE_KEY").unwrap().as_str(),
     );
-    let emails = build_emails().await?;
+    let emails = build_reset_email(reseter.into_inner());
+
+    println!(
+        "Mailjet response {:?}",
+        client.send(emails).await.expect("Could not send emails!")
+    );
+
+    return Json(());
+}
+
+fn build_reset_email(reseted : Reset) -> Batch {
+    let mut message = Message::new(
+        "contact@mero.chat",
+        "MeroChat",
+        Some("Password reset on MeroChat!".to_string()),
+        None,
+    );
+    let email_id = Uuid::new_v4().to_string();
+    let mut vars = Map::new();
+
+    vars.insert(String::from("token"), Value::from(reseted.token));
+    vars.insert(String::from("recipient_id"), Value::from(reseted.user_id));
+    vars.insert(String::from("email_id"), Value::from(email_id.clone()));
+
+    message.vars = Some(vars);
+    message.use_mj_template_language = Some(true);
+    message.mj_template_id = Some(7108800);
+    message.push_recipient(Recipient::new(&reseted.email));
+
+    return Batch { messages: vec![message] };
+}
+
+async fn unread_emails() -> Result<(), Error> {
+    let client = Client::new(
+        SendAPIVersion::V3,
+        env::var("MAILJET_PUBLIC_KEY").unwrap().as_str(),
+        env::var("MAILJET_PRIVATE_KEY").unwrap().as_str(),
+    );
+    let emails = build_unread_emails().await?;
 
     println!("Checking for emails to send...");
 
@@ -26,13 +105,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         println!("No emails to send");
     } else {
         database::insert_unsubscribe_tokens(&emails.messages).await?;
-        println!("Mailjet response {:?}", client.send(emails).await.expect("Could not send emails!"));
+        println!(
+            "Mailjet response {:?}",
+            client.send(emails).await.expect("Could not send emails!")
+        );
     }
 
-    Ok(())
+    return Ok(());
 }
 
-async fn build_emails() -> Result<Batch, Error> {
+async fn build_unread_emails() -> Result<Batch, Error> {
     let users = database::select_users_with_unread_messages().await?;
     let mut emails = Vec::new();
 
@@ -50,13 +132,16 @@ async fn build_emails() -> Result<Batch, Error> {
             "contact@mero.chat",
             "MeroChat",
             Some("You have unread messages on MeroChat!".to_string()),
-            None
+            None,
         );
         let email_id = Uuid::new_v4().to_string();
         let mut vars = Map::new();
 
         vars.insert(String::from("unread_count"), Value::from(count));
-        vars.insert(String::from("user_names"), Value::from(user_names.join(", ")));
+        vars.insert(
+            String::from("user_names"),
+            Value::from(user_names.join(", ")),
+        );
         vars.insert(String::from("recipient_id"), Value::from(id));
         vars.insert(String::from("email_id"), Value::from(email_id.clone()));
 
@@ -77,9 +162,6 @@ struct Batch {
 
 impl Payload for Batch {
     fn to_json(&self) -> String {
-        return format!(
-            "{{ \"Messages\": {} }}",
-            to_string(&self.messages).unwrap()
-        );
+        return format!("{{ \"Messages\": {} }}", to_string(&self.messages).unwrap());
     }
 }
